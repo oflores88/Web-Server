4b5a4a9747ab2922e586a82965e25178ba96a0f9
Oscar <oscarnoe1988@gmail.com>
2017-05-11 21:42:33 -0400

    created my first server

--- server.rb
+++ server.rb
@@ -1,19 +1,15 @@
 require 'sinatra'
 
 get '/' do
-  "Hello world"
+  # "Hello world"
+  File.read('index.html')
 end
 
 get "/sinatra" do
   "Hello Sinatra"
 end
 
-get "/teams" do
-  teams = ["Barcelona", "Malaga", "Atletico", "Villareal", "Real Madrid"]
-  teams.each do |team|
-    "#{team}\n"
-    puts team
-  end
-  
-  # "Barcelona, Malaga, Atletico, Villareal, Real Madrid"
+get "/portfolio" do
+  File.read('myapps.html')
+
 end
